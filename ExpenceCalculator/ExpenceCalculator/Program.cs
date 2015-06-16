9773314dea40e4e4002a4e86dc91b362459cbd84
anantde <anant.deshpande@intellect.edu.in>
2015-06-16 19:20:08 +0530

    expence calculator console app

---
+++ ExpenceCalculator/ExpenceCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables for holding different expenses
+            int food;
+            int transport;
+            int miscellaneous;
+            int totalExpense = 0;
+            string message = "Your total expense for today is : ";
+
+            //Accept expense values from the user console
+            Console.WriteLine("Enter expense for food:");
+            food = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter expense for transport:");
+            transport = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter miscellaneous expense:");
+            miscellaneous = int.Parse(Console.ReadLine());
+
+            totalExpense = food + transport + miscellaneous;
+            Console.WriteLine(message + totalExpense);
+            //A more elegant way of writing the above line
+
+            //Console.WriteLine ("Your total expense for today is :{0} ", totalExpense);
+        }
+    }
+}

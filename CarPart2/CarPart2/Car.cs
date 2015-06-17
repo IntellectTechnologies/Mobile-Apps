088c9847c668188a18a0adaf64da6e655f11ae14
anantde <anant.deshpande@intellect.edu.in>
2015-06-17 17:15:52 +0530

    Car App part II

---
+++ CarPart2/CarPart2/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPart2
+{
+    class Car
+    {
+        private string _colour;
+        private string _make;
+        private int _year;
+
+
+        public Car(string color, string make, int year)
+        {
+            this._colour = color;
+            this._make = make;
+            this._year = year;
+        }
+
+        public Car()
+        {
+
+        }
+
+        public string Describe()
+        {
+            return "This car is " + Colour;
+        }
+
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+    }
+}
---
+++ CarPart2/CarPart2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car("Pearl Black", "Mercedes S5", 2012);
+
+
+            //			firstCar.Colour="Pearl Black";
+            //			firstCar.Make="Mercedes S5";
+            //			firstCar.Year = 2012;
+
+            Console.WriteLine("First Car:");
+            Console.WriteLine("\n");
+            Console.WriteLine("Colour:{0}", firstCar.Colour);
+            Console.WriteLine("Make:{0}", firstCar.Make);
+            Console.WriteLine("Year:{0}", firstCar.Year);
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            Car secondCar = new Car("Azure", "Pontiac", 1990);
+
+            //			secondCar.Colour="Azure";
+            //			secondCar.Make="Pontiac";
+            //			secondCar.Year = 1990;
+
+            Console.WriteLine("Second Car:");
+            Console.WriteLine("\n");
+            Console.WriteLine("Colour:{0}", secondCar.Colour);
+            Console.WriteLine("Make:{0}", secondCar.Make);
+            Console.WriteLine("Year:{0}", secondCar.Year);
+
+
+
+        }
+    }
+}

70395921fbf88f4e79c072e67ad8d784db0e9da7
anantde <anant.deshpande@intellect.edu.in>
2015-06-17 17:06:45 +0530

    Car app

---
+++ CarApp/CarApp/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class Car
+    {
+        public string colour;
+        public string make;
+        public int year;
+
+
+    }
+}

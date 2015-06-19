668cae5e216d26af39c4d3f1d1dab0545662042d
anantde <anant.deshpande@intellect.edu.in>
2015-06-19 17:57:21 +0530

    Debugging

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color mycolor =  new Color();
+            String color = mycolor.DarkGreen;
+            Console.WriteLine("The colour is :{0}", color);
+
+            Console.ReadLine();
+
+        }
+    }
+}

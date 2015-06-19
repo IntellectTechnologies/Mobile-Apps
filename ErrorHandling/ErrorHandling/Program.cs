302c3e03a6c897d1bd1dc18f74386a70168fe60e
anantde <anant.deshpande@intellect.edu.in>
2015-06-19 18:17:25 +0530

    Error Handling

---
+++ ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //			
+            //			try
+            //			{
+            //				int divByZero = 42 / int.Parse("0");
+            //			}
+            //			catch (DivideByZeroException ex)
+            //			{
+            //				
+            //				Console.WriteLine (ex.Message.ToString ());
+            //			}
+
+
+
+            try
+            {
+                using (var text = File.OpenText("saved_game001.txt"))
+                {
+                    Console.WriteLine("{0}", text.ReadLine());
+
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Insights.Report(ex, new Dictionary<string, string>
+					{
+						{ "Filename", "saved_game001.txt" },
+						{ "Where", "Reload game" },
+						{ "Issue", "Index of available games is corrupted" }
+					});
+            }
+
+
+
+        }
+    }
+}
